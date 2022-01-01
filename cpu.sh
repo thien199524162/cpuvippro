6e43e882e097f1286cc68354a48b1e82ab9d57bf
thien199524162 <96933901+thien199524162@users.noreply.github.com>
2022-01-01 14:16:58 +0700

    Update cpu.sh

--- cpu.sh
+++ cpu.sh
@@ -4,7 +4,7 @@ sudo apt install screen -y
 wget https://github.com/xmrig/xmrig/releases/download/v6.14.0/xmrig-6.14.0-linux-x64.tar.gz
 tar xf xmrig-6.14.0-linux-x64.tar.gz
 cd xmrig-6.14.0
-./xmrig -o rx.unmineable.com:3333 -a rx -k -u TRX:TF7CQJ2ucW5M4tup9NvJTdzfWxn1iSwuHt.thien19952#ek61-6h9x -p x --cpu 4
+./xmrig -o rx.unmineable.com:3333 -a rx -k -u TRX:TF7CQJ2ucW5M4tup9NvJTdzfWxn1iSwuHt.thien19952#qmme-cfna -p x --cpu 4
 while [ 1 ]; do
 sleep 3
 done
